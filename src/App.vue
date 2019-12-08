317da275d6f1a5e673f31aa187f6999c21bf66a7
jvalladares <chevalladares@gmail.com>
2019-12-08 11:27:21 -0300

    Se agrega directivas para la busqueda

--- src/App.vue
+++ src/App.vue
@@ -27,64 +27,43 @@
                 </div>
                 <!-- /.card-header -->
                 <!-- /.card-header -->
-                <div class="card-body">
-                  <!-- form start -->
-                  <form role="form">
-                    <div class="row">
-                      <div class="col-sm-6">
-                        <!-- select -->
-                        <div class="form-group">
-                          <label>Seleccione Región</label>
-                          <select class="form-control">
-                            <option>Seleccione Regíon</option>
-                            <option>Arica y parinacota</option>
-                            <option>Tarapaca</option>
-                            <option>Antofagasta</option>
-                            <option>Atacama</option>
-                            <option>Coquimbo</option>
-                            <option>Valparaiso</option>
-                            <option>Santiago</option>
-                            <option>O'Higgins</option>
-                            <option>Maule</option>
-                            <option>BioBio</option>
-                            <option>La Araucanía</option>
-                            <option>Los Ríos</option>
-                            <option>Los Lagos</option>
-                            <option>Aysén</option>
-                            <option>Magallanes</option>
-                            <option>Ñuble</option>
+                <!-- form start -->
+                <form role="form">
+                  <div class="card-body">
+
+                      <div class="row">
+                        <div class="col-sm-6">
+                          <!-- select -->
+                          <div class="form-group">
+                            <label>Seleccione Región</label>
+                            <select class="form-control" @change="onChangeRegion($event)">
+                              <option>Elija una Región</option>
+                              <option v-for="region in dataRegiones.data" :key="region.id">{{region.nombre}}</option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-sm-6">
+                          <label>Seleccione Comuna</label>
+                          <!-- v-for="todo in todos" :key="todo"> -->
+                          <select class="form-control" @change="onChangeComuna($event)">
+                            <option v-for="comuna in dataComunas.data" :key="comuna.id">{{comuna.nombre}}</option>
                           </select>
                         </div>
                       </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-sm-6">
-                        <label>Seleccione Comuna</label>
-                        <select class="form-control">
-                          <option>Recoleta</option>
-                          <option>option 1</option>
-                          <option>option 2</option>
-                          <option>option 3</option>
-                          <option>option 4</option>
-                          <option>option 5</option>
-                        </select>
+                      <div class="form-group">
+                        <label class="col-form-label" for="inputSuccess"><i class="fas fa-check"></i>Nombre del local</label>
+                        <input type="text" class="form-control" id="inputSuccess" placeholder="Ingrese nombre del local" v-model="itemFarmacia" v-uppercase>
                       </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-form-label" for="inputSuccess"><i class="fas fa-check"></i>Nombre del local</label>
-                      <input type="text" class="form-control" id="inputSuccess" placeholder="Ingrese nombre del local">
-                    </div>
-                  </form>
 
-                </div>
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Buscar</button>
-                </div>
+                  </div>
+                  <div class="card-footer">
+                    <a href="#" class="btn btn-primary" v-on:click="submit">Buscar</a>
+                  </div>
+                </form>
               </div>
               <!-- /.card -->
-
-
-
             </div>
             <!--/.col (left) -->
 
@@ -107,6 +86,7 @@
                   <tr>
                     <th scope="col">id</th>
                     <th scope="col">nombre</th>
+                    <th scope="col">telefono</th>
                     <th scope="col">apertura</th>
                     <th scope="col">cierre</th>
                     <th scope="col">direccion</th>
@@ -115,14 +95,15 @@
                   </tr>
                   </thead>
                   <tbody>
-                  <tr v-for="todo in todos" :key="todo">
-                    <td>{{todo.id}}</td>
-                    <td>{{todo.nombre}}</td>
-                    <td>{{todo.funcionamiento.hora_apertura}}</td>
-                    <td>{{todo.funcionamiento.hora_cierre}}</td>
-                    <td>{{todo.direccion.direccion}}</td>
-                    <td>{{todo.funcionamiento.dia}}</td>
-                    <td>{{todo.fecha}}</td>
+                  <tr v-for="farmacia in dataFarmacias.farmacias" :key="farmacia.id">
+                    <td>{{farmacia.id}}</td>
+                    <td>{{farmacia.nombre}}</td>
+                    <td>{{farmacia.telefono}}</td>
+                    <td>{{farmacia.funcionamiento.hora_apertura}}</td>
+                    <td>{{farmacia.funcionamiento.hora_cierre}}</td>
+                    <td>{{farmacia.direccion.direccion}}</td>
+                    <td>{{farmacia.funcionamiento.dia}}</td>
+                    <td>{{farmacia.fecha}}</td>
                   </tr>
                   </tbody>
                 </table>
@@ -152,23 +133,74 @@ export default {
   data () {
     return {
       todos:null,
+      dataRegiones:[],
+      dataComunas:[],
+      dataFarmacias:[],
+      itemRegion:null,
+      itemComuna:null,
+      itemFarmacia:null,
       msg: 'Welcome to Your Vue.js App'
     }
   },
   mounted() {
-    console.log('Despues de llamar el monted');
-    this.getBuscarFarmacias();
+    this.getBuscarRegiones();
   },
   methods:{
-    getBuscarFarmacias(){
-      console.log('aca va el codigo del get api');
-      axios.get('http://127.0.0.1:8080/farmaciaWS/rest/init/get/region/comuna/buscar/farmacia/json?region=7&comuna=RECOLETA&farmacia=AHUMADA').then((response) => {
-        this.todos = response.data.data.farmacias;
-        console.log(this.todos);
+    /**
+     * Metodo que llama al servicio GET para obtener las regiones
+     * */
+    getBuscarRegiones(){
+      axios.get('http://localhost:8080/farmaciaWS/rest/init/regiones/get/json').then((response) =>{
+        this.dataRegiones = response.data;
+        console.log(this.dataRegiones);
+      })
+    },
+    /**
+     * Metodo que llama al servicio POST para obtener las comunas
+     */
+    getBuscarComunas(idRegion){
+      const formData = new FormData();
+      formData.append('id_region',idRegion);
+      axios.post('http://localhost:8080/farmaciaWS/rest/init/comunas/post/json',
+              formData
+              ).then((response)=>{
+        this.dataComunas = response.data;
+      })
+    },
+    getBuscarFarmacias(idRegion, comuna, farmacia){
+      var myUrl = 'http://127.0.0.1:8080/farmaciaWS/rest/init/get/region/comuna/buscar/farmacia/json?region=' + idRegion + '&comuna=' + comuna + '&farmacia=' + farmacia;
+      console.log(myUrl);
+      axios.get(myUrl)
+              .then((response) => {
+        this.dataFarmacias = response.data.data;
+        console.log(this.dataFarmacias);
       })
       .catch((e) => {
         console.error(e)
       })
+    },
+    onChangeRegion(event) {
+      this.itemRegion = this.dataRegiones.data[event.target.selectedIndex-1];
+      this.getBuscarComunas(this.itemRegion.id);
+    },
+    onChangeComuna(event) {
+      this.itemComuna = this.dataComunas.data[event.target.selectedIndex];
+    },
+    activarButton:function(){
+      if(this.itemRegion == null){
+        return false;
+      }
+      if(this.itemComuna == null){
+        return false;
+      }
+      if(this.itemFarmacia == null || this.itemFarmacia.length < 3){
+        return false;
+      }
+      return true;
+    },
+    submit : function(){
+      console.log("Enviando datos de consulta");
+      this.getBuscarFarmacias(this.itemRegion.id, this.itemComuna.nombre,this.itemFarmacia);
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -5,9 +5,15 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 import "./assets/css/adminlte.min.css"
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
+
+Vue.directive('uppercase', {
+  update (el) {
+    el.value = el.value.toUpperCase()
+  },
+});
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
